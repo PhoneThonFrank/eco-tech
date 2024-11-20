1313ee2cacd57dee5a04e4a66a5c04cae14c9574
PhoneThonFrank <107785683+PhoneThonFrank@users.noreply.github.com>
2024-11-20 17:06:54 +0630

    Add files via upload

---
+++ contact_us/script.js
@@ -0,0 +1,8 @@
+// function clearInput(){
+//     document.getElementById('result').innerHTML = 'Your message has been sent.'
+// }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
